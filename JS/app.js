4d111b5a956f8806cec55b1861167cfe55dd23a9
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-07-13 15:53:24 +0900

    Day 9 of 15__07/13 failed, 해결
    parseInt(value)를 안해주면 string이 된다
    식을 함수에 안넣고 바로 const에 넣었다.
    진짜 아쉽다... 진짜 2%가 부족했다.

--- JS/app.js
+++ JS/app.js
@@ -1,38 +1,25 @@
-const set = document.querySelector("#set input");
-const blockToRefresh = document.querySelector("#guess-form"); //form 전체를 선언해야함.
+const guessForm = document.querySelector("#guess-form");
+const set = document.querySelector("#set-form input");
 const guessNumber = document.querySelector("#guess-form input");
-const h3 = document.querySelector("h3");
-const chosen = document.querySelector("h3:first-child");
-const yourResult = document.querySelector("h3:last-child");
-const setValue = set.value;
+const selectedNumber = document.querySelector("h3:first-child");
+const result = document.querySelector("h3:last-child");
 
-function result(event) {
+function play(event) {
   event.preventDefault();
-  const userNumber = parseInt(guessNumber.value); //value로 넘버를 읽힘
-  const machineNumber = setRandomNumber();
-  chosen.innerText = `Your chose: ${userNumber}, the machine chose: ${machineNumber}`;
-  if (machineNumber < 0) {
-    alert("Please set positive number");
-    yourResult.classList.add("none");
-    chosen.classList.add("none");
-  } else if (userNumber >= setValue) {
+  const setValue = parseInt(set.value);
+  const userNumber = parseInt(guessNumber.value);
+  const machineNumber = 1 + Math.floor(Math.random() * parseInt(setValue));
+  if (setValue < 0) {
+    alert("Please write positive number");
+  } else if (userNumber > setValue) {
     alert("Please follow the rule");
-    yourResult.classList.add("none");
-    chosen.classList.add("none");
   } else if (userNumber === machineNumber) {
-    yourResult.innerText = "You Win!";
-    yourResult.classList.remove("none");
-    chosen.classList.remove("none");
+    selectedNumber.innerText = `You chose ${userNumber}, the machine chose ${machineNumber}`;
+    result.innerText = "You Win!";
   } else {
-    yourResult.innerText = "You Lost T_T";
-    yourResult.classList.remove("none");
-    chosen.classList.remove("none");
+    selectedNumber.innerText = `You chose ${userNumber}, the machine chose ${machineNumber}`;
+    result.innerText = "You Lose T_T";
   }
 }
 
-function setRandomNumber() {
-  const randomResult = Math.floor(Math.random() * (setValue - 0 + 1) + 0);
-  return randomResult;
-}
-
-blockToRefresh.addEventListener("submit", result);
+guessForm.addEventListener("submit", play);
